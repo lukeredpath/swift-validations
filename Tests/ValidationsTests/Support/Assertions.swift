e49080db6bdd8efd66cd23f44ca5df0b347cae0d
Luke Redpath <luke@lukeredpath.co.uk>
2020-06-30 17:57:52 +0100

    Fix warnings passing #file to #filePath
    (see https://github.com/apple/swift-nio-http2/pull/209)

--- Tests/ValidationsTests/Support/Assertions.swift
+++ Tests/ValidationsTests/Support/Assertions.swift
@@ -4,7 +4,7 @@ import Validations
 func assertValid<Value>(
     _ validator: ValidatorOf<Value, String>,
     given value: Value,
-    file: StaticString = #filePath,
+    file: StaticString = (#file),
     line: UInt = #line
 ) {
     let result = validator.validate(value)
@@ -18,7 +18,7 @@ func assertValid<Value>(
 func assertNotValid<Value>(
     _ validator: ValidatorOf<Value, String>,
     given value: Value,
-    file: StaticString = #filePath,
+    file: StaticString = (#file),
     line: UInt = #line
 ) {
     let result = validator.validate(value)
@@ -33,7 +33,7 @@ func assertNotValid<Value, Error>(
     _ validator: ValidatorOf<Value, Error>,
     given value: Value,
     errors: [Error],
-    file: StaticString = #filePath,
+    file: StaticString = (#file),
     line: UInt = #line
 ) where Error: Comparable {
     let result = validator.validate(value)
