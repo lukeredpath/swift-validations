afc8e56a6df59fde3941012555bdb83e695eb766
Luke Redpath <luke@lukeredpath.co.uk>
2020-07-01 14:57:26 +0100

    Fix assertions to work correctly with Swift 5.2 and 5.3.
    Unfortunately whilst wrapping #file in () fixes the warning it still breaks the line highlighting functionality when forwarded into XCTest.
    
    We *need* to use #file on < 5.3 and #filePath on >= 5.3.
    
    This should hopefully change soon:
    https://forums.swift.org/t/revisiting-the-source-compatibility-impact-of-se-0274-concise-magic-file-names/37720/3

--- Tests/ValidationsTests/Support/Assertions.swift
+++ Tests/ValidationsTests/Support/Assertions.swift
@@ -1,10 +1,10 @@
 import XCTest
 import Validations
 
-func assertValid<Value>(
+func _assertValid<Value>(
     _ validator: ValidatorOf<Value, String>,
     given value: Value,
-    file: StaticString = (#file),
+    filePath: StaticString,
     line: UInt = #line
 ) {
     let result = validator.validate(value)
@@ -12,13 +12,13 @@ func assertValid<Value>(
     XCTAssert(
         result.isValid,
         "should be valid given \(value)",
-        file: file, line: line)
+        file: filePath, line: line)
 }
 
-func assertNotValid<Value>(
+func _assertNotValid<Value>(
     _ validator: ValidatorOf<Value, String>,
     given value: Value,
-    file: StaticString = (#file),
+    filePath: StaticString,
     line: UInt = #line
 ) {
     let result = validator.validate(value)
@@ -26,24 +26,84 @@ func assertNotValid<Value>(
     XCTAssertFalse(
         result.isValid,
         "should not be valid given \(value)",
-        file: file, line: line)
+        file: filePath, line: line)
 }
 
-func assertNotValid<Value, Error>(
+func _assertNotValid<Value, Error>(
     _ validator: ValidatorOf<Value, Error>,
     given value: Value,
     errors: [Error],
-    file: StaticString = (#file),
+    filePath: StaticString,
     line: UInt = #line
 ) where Error: Comparable {
     let result = validator.validate(value)
     
     XCTAssertFalse(
         result.isValid, "should not be valid given \(value)",
-        file: file, line: line)
+        file: filePath, line: line)
     
     XCTAssertEqual(
         errors.sorted(), Array(result.errors!),
         "expected errors to match: \(errors)",
-        file: file, line: line)
+        file: filePath, line: line)
 }
+
+// Remove this once the current source-compatibility breaking change is fixed, hopefully
+// before Xcode 12 final drops!
+//
+// See: https://forums.swift.org/t/revisiting-the-source-compatibility-impact-of-se-0274-concise-magic-file-names/37720/3
+
+#if compiler(>=5.3)
+func assertValid<Value>(
+    _ validator: ValidatorOf<Value, String>,
+    given value: Value,
+    filePath: StaticString = #filePath,
+    line: UInt = #line
+) {
+    _assertValid(validator, given: value, filePath: filePath, line: line)
+}
+func assertNotValid<Value>(
+    _ validator: ValidatorOf<Value, String>,
+    given value: Value,
+    filePath: StaticString = #filePath,
+    line: UInt = #line
+) {
+    _assertNotValid(validator, given: value, filePath: filePath, line: line)
+}
+func assertNotValid<Value, Error>(
+    _ validator: ValidatorOf<Value, Error>,
+    given value: Value,
+    errors: [Error],
+    filePath: StaticString = #filePath,
+    line: UInt = #line
+) where Error: Comparable {
+    _assertNotValid(validator, given: value, errors: errors, filePath: filePath, line: line)
+}
+#else
+func assertValid<Value>(
+    _ validator: ValidatorOf<Value, String>,
+    given value: Value,
+    filePath: StaticString = #file,
+    line: UInt = #line
+) {
+    _assertValid(validator, given: value, filePath: filePath, line: line)
+}
+func assertNotValid<Value>(
+    _ validator: ValidatorOf<Value, String>,
+    given value: Value,
+    filePath: StaticString = #file,
+    line: UInt = #line
+) {
+    _assertNotValid(validator, given: value, filePath: filePath, line: line)
+}
+func assertNotValid<Value, Error>(
+    _ validator: ValidatorOf<Value, Error>,
+    given value: Value,
+    errors: [Error],
+    filePath: StaticString = #file,
+    line: UInt = #line
+) where Error: Comparable {
+    _assertNotValid(vlidator, given: value, errors: errors, filePath: filePath, line: line)
+}
+#endif
+
